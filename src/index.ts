fcbc857fe728cf4549822adb54f13b509038116a
noeldiazven <celso.diaz.ventura@gmail.com>
2026-08-17 22:58:25 -0500

    creando endpoints y corriendo servidor

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
+import express from 'express';
+import type {Request, Response} from "express";
 import fs from "node:fs/promises";
 import path from "node:path";
 
+const app = express();
+const PORT = 3000;
+
+app.get('/api/status', (req:Request,res:Response) => {
+  res.json({
+    status: 'Servidor en línea',
+    version: '1.0.0'
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
